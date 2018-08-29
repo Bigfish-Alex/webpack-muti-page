b64ee7328394a67729738cb5814a9e14af2fce2d
Bigfish-Alex <632388230@qq.com>
2018-08-29 22:30:18 +0800

    多页面打包新增 REM H5 PAGE

---
+++ src/pages/remH5/remH5.vue
@@ -0,0 +1,20 @@
+<template>
+
+</template>
+
+<script>
+import Header from '../../components/Header';
+import Logo from '../../components/Logo';
+
+export default {
+  name: 'remH5',
+  components: {
+    TopHeader: Header,
+    Logo,
+  },
+};
+</script>
+
+<style lang="scss">
+  @import './remH5.css';
+</style>
